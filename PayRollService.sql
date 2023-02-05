2c3f053fa6b3e8de945c898b30034506320bb5af
Divya <beingdivyadixit@gmail.com>
2023-02-05 23:22:14 +0530

    Added Ability to create a payroll service database Uc-1

--- PayRollService.sql
+++
@@ -1,3 +0,0 @@
----------------------UC-1-create database----------------
-create database PayRollService;
-use PayRollService;
---
+++ payroll_Service.sql
@@ -0,0 +1,3 @@
+---------------UC-1-create databse-----------------
+create database payroll_Service_;
+use payroll_Service_;
